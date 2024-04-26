9812cfe7597388d4393344f920d3548fb6740d84
Ali Aliyev <elvinaliyev724@gmail.com>
2024-04-26 20:16:24 +0400

    finish

---
+++ ConsoleApp32/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] adlar = { "Kamal","Ali","Pasa" };
+            Console.Write("Bir herf giriniz :");
+            char b = Convert.ToChar(Console.ReadLine()); ;
+            int check = 0;
+
+            int location = 0;
+            for (int i = 0; i < adlar.Length; i++)
+            {
+                for (int j = 0; j < adlar[i].Length; j++)
+                {
+                    if (b == adlar[i][j])
+                    {
+                        location = j ;
+                        Console.WriteLine(adlar[i] + " - " + b + " elementi bu yerdedir : " + location);
+                    }
+                }
+
+            } 
+        }
+    }
+}
